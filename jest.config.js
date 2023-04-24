4b5553dd0dc53c178640544832fa5c6810ae91e0
Beatriz Mayorka de Aguiar <47663726+beatrizmayorka@users.noreply.github.com>
2023-04-23 23:54:19 -0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   verbose: true,
   testMatch: ['**/*.test.js'],
+  coverageDirectory: './coverage',
+  coverageReporters: ['lcov', 'text'],
+  testResultsProcessor: '@sonarqube/community-sonarqube-javascript'
 };
